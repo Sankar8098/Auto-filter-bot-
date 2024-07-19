b4a41840bb5f8ac61cb3bc47e2533c221a287936
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-19 12:11:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://rebecca9710:CSWG3pOAVs2r4w4b@autofilter2.fa5sbap.mongodb.net/?retryWrites=true&w=majority&appName=Autofilter2")
 DATABASE_NAME = environ.get('DATABASE_NAME', "public")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'TonyStarkcollection')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Premium And Referal Settings
 PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', True)) # Set Ture Or False
